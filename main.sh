34779758550f62e74b2d87349811036938c0696d
Ángel Antonio <91348207+elAgal@users.noreply.github.com>
2021-09-24 15:36:53 -0500

    Uploading some more info
    This are shell scripting files

---
+++ main.sh
@@ -0,0 +1,9 @@
+echo "Hello, this is the main file"
+if [ -f ./routine.sh ]; then
+	echo "There is other file"
+	source ./routine.sh
+	func1 "Hola" "Mi amor"
+else
+	echo "It seems there is not other file"
+fi
+echo "Now we can continue with the setup"
---
+++ routine.sh
@@ -0,0 +1,4 @@
+function func1(){
+	echo "This is a routine from other file that i'm execting"
+	echo "you entered the strings "+"$1"+"$2"
+}
